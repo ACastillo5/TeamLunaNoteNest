c9a7cf3881adc861b76db5a13a8b3e56613de446
Adrian Castillo <adrian.castillo5@aol.com>
2025-03-27 11:33:18 -0400

    Completed Home Page, Upload Page, and headers to all pages

--- guestHead.js
+++ guestHead.js
@@ -2,7 +2,7 @@ const navbar = document.getElementById("topbar");
 navbar.innerHTML += '<div class="heading">' +
     '<h1 class="no-underline" href="">NoteNest</h1>' +
     '<div class="actions">' +
-      '<a href="/signup" class="no-underline">LOG IN</a>' +
+      '<a href="/login" class="no-underline">LOG IN</a>' +
       '<a href="/signup" class="small-btn">SIGN UP</a>' +
     '</div>' +
   '</div>';

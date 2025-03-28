6311981e030c23e040c111506aec9423fc692ebf
Adrian Castillo <adrian.castillo5@aol.com>
2025-03-28 10:16:20 -0400

    Merge with current

--- accountHead.js
+++ accountHead.js
@@ -1,6 +1,6 @@
 const navbar = document.getElementById("topbar");
 navbar.innerHTML += '<div class="heading">' +
-                      '<h1 class="no-underline" href="">NoteNest</h1>' +
+                      '<h1 class="no-underline" href="#">NoteNest</h1>' +
                       '<div class="actions">' +
                         '<a href="/signup" class="no-underline">Search Notes</a>' +
                         '<a href="/upload" class="no-underline">Upload</a>' +
--- guestHead.js
+++ guestHead.js
@@ -1,6 +1,6 @@
 const navbar = document.getElementById("topbar");
 navbar.innerHTML += '<div class="heading">' +
-    '<h1 class="no-underline" href="">NoteNest</h1>' +
+    '<h1 class="no-underline" href="/">NoteNest</h1>' +
     '<div class="actions">' +
       '<a href="/login" class="no-underline">LOG IN</a>' +
       '<a href="/signup" class="small-btn">SIGN UP</a>' +

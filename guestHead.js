846fb272e5029b397f6c44a3b044dce92a9a4067
embrub <embraiguess@gmail.com>
2025-03-29 19:29:13 -0400

    css error fix

--- guestHead.js
+++
@@ -1,8 +0,0 @@
-const navbar = document.getElementById("topbar");
-navbar.innerHTML += '<div class="heading">' +
-    '<h1 class="no-underline" href="">NoteNest</h1>' +
-    '<div class="actions">' +
-      '<a href="/login" class="no-underline">LOG IN</a>' +
-      '<a href="/signup" class="small-btn">SIGN UP</a>' +
-    '</div>' +
-  '</div>';

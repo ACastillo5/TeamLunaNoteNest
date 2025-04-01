cec1424529a8a7d5041eb599ee09e1d67d819cfb
Adrian Castillo <adrian.castillo5@aol.com>
2025-04-01 16:06:30 -0400

    Fixed ejs error

--- app.js
+++ app.js
@@ -9,54 +9,51 @@ app.set('view engine', 'ejs');
 // Serve static files (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname)));
 
-app.get('/preview', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/notes/preview.html'));
-});
-
-app.get('/search', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/notes/search.html'));
-});
-
-app.get('/upload', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/notes/upload.html'));
-});
-
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/user/login.html'));
-});
-
-app.get('/profile', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/user/profile.html'));
-});
-
-app.get('/signup', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/user/signup.html'));
-});
-
-app.get('/profHomePage', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/professor-home.html'));
-});
-
-app.get('/studentHomePage', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/student-home.html'));
-});
-
-app.get('/test', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/user/test.html'));
-});
-
-app.get('/bookmarks', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/user/bookmarks.html'));
-});
-
-app.get('/reports', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/notes/reports.html'));
-});
-
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.render('index');
 });
 
+app.get("/search", (req, res) => {
+  res.render("notes/search"); // Render notes/search.ejs
+});
+
+app.get("/upload", (req, res) => {
+  res.render("notes/upload"); // Render notes/upload.ejs
+});
+
+app.get("/login", (req, res) => {
+  res.render("user/login"); // Render user/login.ejs
+});
+
+app.get("/profile", (req, res) => {
+  res.render("user/profile"); // Render user/profile.ejs
+});
+
+app.get("/signup", (req, res) => {
+  res.render("user/signup"); // Render user/signup.ejs
+});
+
+app.get("/profHomePage", (req, res) => {
+  res.render("professor-home"); // Render professor-home.ejs
+});
+
+app.get("/studentHomePage", (req, res) => {
+  res.render("student-home"); // Render student-home.ejs
+});
+
+app.get("/test", (req, res) => {
+  res.render("user/test"); // Render user/test.ejs
+});
+
+app.get("/bookmarks", (req, res) => {
+  res.render("user/bookmarks"); // Render user/bookmarks.ejs
+});
+
+app.get("/reports", (req, res) => {
+  res.render("notes/reports"); // Render notes/reports.ejs
+});
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
